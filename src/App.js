c1c9622a991fba535314feb47496e78a796e5b48
INonns <thesamgameyt@gmail.com>
2026-04-03 17:05:49 -0500

    Un poco de CSS (04 Class)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
@@ -16,8 +15,7 @@ const defaultTodos = [
 
 function App() {
   return (
-    <React.Fragment>
-
+    <>
       <TodoCounter completed={16} total={25} />
       <TodoSearch />
 
@@ -32,7 +30,7 @@ function App() {
       </TodoList>
 
       <CreateTodoButton/>
-    </React.Fragment>
+    </>
   );
 }
 
