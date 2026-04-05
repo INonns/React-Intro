9cfe05abc5b6ef172f7a21138318f33a9bee8502
INonns <thesamgameyt@gmail.com>
2026-04-05 12:12:23 -0500

    Special Message with all tasks completed (10rq Class)

--- src/Components/01-Header/TodoCounter.js
+++ src/Components/01-Header/TodoCounter.js
@@ -1,9 +1,20 @@
 import './TodoCounter.css'
 
-function TodoCounter({completed, total}) {
+function TodoCounter({total, completed}) {
+
+  let message;
+
+  if (total === 0) {
+    message = "Start creating your tasks"
+  } else if (total === completed) {
+    message = "Congrats, You've completed at all"
+  } else {
+    message = `${total} Tasks - ${completed} Completed`
+  }
+
   return (
     <span id="TodoCounter">
-        {total} Tasks - {completed} Completed
+      {message}
     </span>
   );
 }
